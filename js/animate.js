176055877de8543ab6fbd67f7c4a6a1c61d126b0
urieloliveira <urielsateles@gmail.com>
2018-05-31 16:04:45 -0300

    Adicionando Habilidades - section habilidades

--- js/animate.js
+++ js/animate.js
@@ -17,3 +17,16 @@ const titulo = document.querySelector('h1');
 typeWriter(titulo);
 
 setTimeout(() => document.querySelector('h5').classList.add("animateSurgir"), 3330);
+
+ function mudaTamanho(){
+    var qtd = (document.getElementById('habilidades-content').children.length)/2;
+    for(var i = 0; i < qtd; i++){
+        var tam = (document.getElementsByClassName("porcentagem")[i].innerHTML)/100;
+        document.getElementsByClassName("barra")[i].style.width = `${635*tam}px`;
+    }
+    console.log(qtd);
+}
+
+mudaTamanho(); 
+
+
